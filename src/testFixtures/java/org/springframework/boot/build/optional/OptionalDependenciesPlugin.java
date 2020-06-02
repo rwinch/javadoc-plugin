a88990594841b9b7f5d8cc1e630d9d8181daabe2
Rob Winch <rwinch@users.noreply.github.com>
2020-06-02 09:28:30 -0500

    Change OptionalDependenciesPlugin to extends runtime/compile configurations

--- src/testFixtures/java/org/springframework/boot/build/optional/OptionalDependenciesPlugin.java
+++ src/testFixtures/java/org/springframework/boot/build/optional/OptionalDependenciesPlugin.java
@@ -19,13 +19,9 @@ package org.springframework.boot.build.optional;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.ConfigurationContainer;
 import org.gradle.api.attributes.Usage;
 import org.gradle.api.plugins.JavaPlugin;
-import org.gradle.api.plugins.JavaPluginConvention;
-import org.gradle.api.tasks.SourceSetContainer;
-import org.gradle.api.tasks.javadoc.Javadoc;
-import org.gradle.plugins.ide.eclipse.EclipsePlugin;
-import org.gradle.plugins.ide.eclipse.model.EclipseModel;
 
 /**
  * A {@code Plugin} that adds support for Maven-style optional dependencies. Creates a new
@@ -44,22 +40,14 @@ public class OptionalDependenciesPlugin implements Plugin<Project> {
 
 	@Override
 	public void apply(Project project) {
-		Configuration optional = project.getConfigurations().create(OPTIONAL_CONFIGURATION_NAME);
+		ConfigurationContainer configurations = project.getConfigurations();
+		Configuration optional = configurations.create(OPTIONAL_CONFIGURATION_NAME);
 		optional.attributes((attributes) -> attributes.attribute(Usage.USAGE_ATTRIBUTE,
 				project.getObjects().named(Usage.class, Usage.JAVA_RUNTIME)));
 		project.getPlugins().withType(JavaPlugin.class, (javaPlugin) -> {
-			SourceSetContainer sourceSets = project.getConvention().getPlugin(JavaPluginConvention.class)
-					.getSourceSets();
-			sourceSets.all((sourceSet) -> {
-				sourceSet.setCompileClasspath(sourceSet.getCompileClasspath().plus(optional));
-				sourceSet.setRuntimeClasspath(sourceSet.getRuntimeClasspath().plus(optional));
-			});
-			project.getTasks().withType(Javadoc.class)
-					.all((javadoc) -> javadoc.setClasspath(javadoc.getClasspath().plus(optional)));
+			configurations.getByName(JavaPlugin.RUNTIME_CLASSPATH_CONFIGURATION_NAME).extendsFrom(optional);
+			configurations.getByName(JavaPlugin.COMPILE_CLASSPATH_CONFIGURATION_NAME).extendsFrom(optional);
 		});
-		project.getPlugins().withType(EclipsePlugin.class,
-				(eclipsePlugin) -> project.getExtensions().getByType(EclipseModel.class)
-						.classpath((classpath) -> classpath.getPlusConfigurations().add(optional)));
 	}
 
 }
